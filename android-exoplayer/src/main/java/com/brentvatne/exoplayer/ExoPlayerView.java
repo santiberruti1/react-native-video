ff162a3fff7a5f7f99cb0ee28e947e0f8219afcc
santiago berruti <santiago.berruti@fing.edu.uy>
2021-04-19 15:48:24 -0300

    a
    a

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
@@ -17,6 +17,7 @@ import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.video;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.text.Cue;
@@ -205,7 +206,7 @@ public final class ExoPlayerView extends FrameLayout {
         layout.invalidateAspectRatio();
     }
 
-    private final class ComponentListener implements SimpleExoPlayer.VideoListener,
+    private final class ComponentListener implements VideoListener,
             TextOutput, ExoPlayer.EventListener {
 
         // TextRenderer.Output implementation
